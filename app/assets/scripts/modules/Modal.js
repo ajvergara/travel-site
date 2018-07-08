148985b5ca94003f5e4051ab706c1f91294a2d8d
ajvergara <ajvergara1014@gmail.com>
2018-07-08 12:58:38 +0300

    Completed Modal Features

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,6 +1,28 @@
+import $ from "jquery";
+
 class Modal{
   constructor(){
+    this.modalItself = $(".modal");
+    this.openModalBtn = $(".open-modal");
+    this.closeModalBtn = $(".modal__close-x");
+    this.body = $("body");
+    this.events();
+  }
 
+  events(){
+    this.openModalBtn.on("click", this.openModalFN.bind(this));
+    this.closeModalBtn.on("click", this.closeModalFN.bind(this));
+  }
+
+  openModalFN(){
+    this.modalItself.addClass("modal--is-visible");
+    this.body.addClass("body--is-fixed");
+    return false;
+  }
+
+  closeModalFN(){
+    this.modalItself.removeClass("modal--is-visible");
+    this.body.removeClass("body--is-fixed");
   }
 }
 
