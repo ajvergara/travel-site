bb556e3124cf6d53e52819a435c1537842cbc0d6
ajvergara <ajvergara1014@gmail.com>
2018-07-08 10:58:05 +0300

    Completed Reveal Item on Feature Items - next make it reuseable

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,3 @@
+import RevealItems from "./modules/RevealItems";
 
-import Person from "./modules/Person";
-
-var joe = new Person("John Joe", "blue");
-joe.greet();
-
-var jane = new Person("Jane Doe", "pink");
-jane.greet();
+const revealItem = new RevealItems();
--- app/assets/scripts/modules/RevealItems.js
+++ app/assets/scripts/modules/RevealItems.js
@@ -1,6 +1,28 @@
+import $ from "jquery";
+import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints.min"
+
 class RevealItems{
   constructor(){
+    this.itemsToReveal = $(".feature-item");
+    this.hideInitially();
+    this.createWaypoints();
+  }
 
+  hideInitially(){
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  createWaypoints(){
+    this.itemsToReveal.each(function(){
+      let currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function(){
+          $(currentItem).addClass("reveal-item--revealed");
+        },
+        offset: "75%"
+      });
+    });
   }
 }
 
